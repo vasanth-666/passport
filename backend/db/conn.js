b91454eac51109403c4b4e5361a2ac21fa06103f
vasanth-666 <vasanthakumar.senthil@gmail.com>
2024-12-23 13:47:54 +0530

    bug fixes

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -2,10 +2,6 @@ const mongoose = require("mongoose");
 
 const DB = process.env.mongodburl;
 
-mongoose.connect(DB,{
-
-    useUnifiedTopology:true,
-    useNewUrlParser:true
-}).then(()=>
+mongoose.connect(DB).then(()=>
     console.log("database connected")).
     catch((err)=>console.log("errr",err))
